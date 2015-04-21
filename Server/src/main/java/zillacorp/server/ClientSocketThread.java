6cf353acefa633fb4560899973a97ce2fd27a419
Lukas Hurtz <lukas@am-nordpark.de>
2015-04-21 21:41:43 +0200

    Thread Klassen für Db-Verbindung, ClientSocket-Verbindung und Server erstellt

---
+++ Server/src/main/java/zillacorp/server/ClientSocketThread.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package zillacorp.server;
+
+/**
+ *
+ * @author l.hurtz
+ */
+public class ClientSocketThread extends Thread implements Runnable
+{
+    public void run()
+    {
+        //TODO
+    }
+}
