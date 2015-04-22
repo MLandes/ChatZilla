8f36667a0f755a21da16a8b35b8b912ee92c56c3
Lukas Hurtz <lukas@am-nordpark.de>
2015-04-22 11:54:20 +0200

    Daten Modell Klassen hinzugefügt

---
+++ Server/src/main/java/zillacorp/models/UserOnline.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package zillacorp.models;
+
+/**
+ *
+ * @author l.hurtz
+ */
+public class UserOnline {
+    public String nickname;   
+}
