fb3ee720db26e04ae67e61aa44295b94818ea39d
Lukas Hurtz <lukas@am-nordpark.de>
2015-04-22 12:51:55 +0200

    Initialisierung des DatenbankThreads implementiert

--- Server/src/main/java/zillacorp/server/DatabaseThread.java
+++ Server/src/main/java/zillacorp/server/DatabaseThread.java
@@ -5,14 +5,71 @@
  */
 package zillacorp.server;
 
+import org.lightcouch.CouchDbClient;
+import org.lightcouch.CouchDbProperties;
+import zillacorp.model.Message;
+
 /**
  *
  * @author l.hurtz
  */
 public class DatabaseThread extends Thread implements Runnable
 {
+    CouchDbClient messageDatabaseClient;
+    CouchDbClient userRegisteredDatabaseClient;
+    CouchDbClient userOnlineDatabaseClient;
+    
+    public DatabaseThread(String databaseIp)
+    {
+        connectToMessageDatabse(databaseIp);
+        connectToUserRegisteredDatabse(databaseIp);
+        connectToUserOnlineDatabse(databaseIp);
+    }
+    
     public void run()
     {
         
+    }
+
+    private void connectToMessageDatabse(String databaseIp)
+    {
+        CouchDbProperties properties = new CouchDbProperties()
+            .setDbName("chatzilla_message-history")
+            .setCreateDbIfNotExist(true)
+            .setProtocol("http")
+            .setHost(databaseIp)
+            .setPort(5984)
+            .setMaxConnections(100)
+            .setConnectionTimeout(0);
+        
+        messageDatabaseClient = new CouchDbClient(properties);
+    }
+    
+    private void connectToUserRegisteredDatabse(String databaseIp)
+    {
+        CouchDbProperties properties = new CouchDbProperties()
+            .setDbName("chatzilla_user-registered")
+            .setCreateDbIfNotExist(true)
+            .setProtocol("http")
+            .setHost(databaseIp)
+            .setPort(5984)
+            .setMaxConnections(100)
+            .setConnectionTimeout(0);
+        
+        userRegisteredDatabaseClient = new CouchDbClient(properties);
+    }
+    
+    private void connectToUserOnlineDatabse(String databaseIp)
+    {
+        CouchDbProperties properties = new CouchDbProperties()
+            .setDbName("chatzilla_user-online")
+            .setCreateDbIfNotExist(true)
+            .setProtocol("http")
+            .setHost(databaseIp)
+            .setPort(5984)
+            .setMaxConnections(100)
+            .setConnectionTimeout(0);
+        
+        userOnlineDatabaseClient = new CouchDbClient(properties);
     }
 }
