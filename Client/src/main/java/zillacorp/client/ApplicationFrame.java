661badd9d3690086405ac66209f69c9b126f975f
Martin Landes <commit@mlandes.de>
2015-04-23 23:38:31 +0200

    Added Client ApplicationFrame

--- Client/src/main/java/zillacorp/client/ApplicationFrame.java
+++ Client/src/main/java/zillacorp/client/ApplicationFrame.java
@@ -16,6 +16,8 @@ public class ApplicationFrame extends javax.swing.JFrame {
      */
     public ApplicationFrame() {
         initComponents();
+        MessageTextField.requestFocus();
+        this.getRootPane().setDefaultButton(SendenButton);
     }
 
     /**
@@ -27,21 +29,133 @@ public class ApplicationFrame extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        ChatRoomTabbedPane = new javax.swing.JTabbedPane();
+        ChatRoomPanel = new javax.swing.JPanel();
+        MessageHistoryScrollPane = new javax.swing.JScrollPane();
+        MessageHistoryTextPane = new javax.swing.JTextPane();
+        TrennenButton = new javax.swing.JButton();
+        OnlineClientsScrollPane = new javax.swing.JScrollPane();
+        OnlineClientsList = new javax.swing.JList();
+        OnlineClientsLabel = new javax.swing.JLabel();
+        MessageTextField = new javax.swing.JTextField();
+        SendenButton = new javax.swing.JButton();
+        StatusSeparator = new javax.swing.JSeparator();
+        StatusLabel = new javax.swing.JLabel();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("ChatZilla");
+        setLocationByPlatform(true);
+
+        MessageHistoryTextPane.setEditable(false);
+        MessageHistoryTextPane.setText("test");
+        MessageHistoryScrollPane.setViewportView(MessageHistoryTextPane);
+
+        TrennenButton.setText("Trennen");
+        TrennenButton.setFocusable(false);
+        TrennenButton.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+        TrennenButton.setVerticalTextPosition(javax.swing.SwingConstants.BOTTOM);
+        TrennenButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                TrennenButtonActionPerformed(evt);
+            }
+        });
+
+        OnlineClientsList.setModel(new javax.swing.AbstractListModel() {
+            String[] strings = { "Du", "User 2", "User 3", "User 4", "User 5" };
+            public int getSize() { return strings.length; }
+            public Object getElementAt(int i) { return strings[i]; }
+        });
+        OnlineClientsScrollPane.setViewportView(OnlineClientsList);
+
+        OnlineClientsLabel.setText("Online Clients");
+
+        SendenButton.setText("Senden");
+        SendenButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                SendenButtonActionPerformed(evt);
+            }
+        });
+
+        StatusLabel.setText("Verbunden mit: ");
+
+        javax.swing.GroupLayout ChatRoomPanelLayout = new javax.swing.GroupLayout(ChatRoomPanel);
+        ChatRoomPanel.setLayout(ChatRoomPanelLayout);
+        ChatRoomPanelLayout.setHorizontalGroup(
+            ChatRoomPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(StatusSeparator, javax.swing.GroupLayout.Alignment.TRAILING)
+            .addGroup(ChatRoomPanelLayout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(ChatRoomPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(ChatRoomPanelLayout.createSequentialGroup()
+                        .addGroup(ChatRoomPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(ChatRoomPanelLayout.createSequentialGroup()
+                                .addComponent(MessageTextField)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(SendenButton))
+                            .addComponent(MessageHistoryScrollPane))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addGroup(ChatRoomPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(OnlineClientsScrollPane, javax.swing.GroupLayout.PREFERRED_SIZE, 120, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, ChatRoomPanelLayout.createSequentialGroup()
+                                .addGroup(ChatRoomPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                                    .addComponent(OnlineClientsLabel)
+                                    .addComponent(TrennenButton))
+                                .addGap(24, 24, 24))))
+                    .addGroup(ChatRoomPanelLayout.createSequentialGroup()
+                        .addComponent(StatusLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 697, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(0, 0, Short.MAX_VALUE)))
+                .addContainerGap())
+        );
+        ChatRoomPanelLayout.setVerticalGroup(
+            ChatRoomPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, ChatRoomPanelLayout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(ChatRoomPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(ChatRoomPanelLayout.createSequentialGroup()
+                        .addComponent(MessageHistoryScrollPane)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addGroup(ChatRoomPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(MessageTextField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(SendenButton)))
+                    .addGroup(ChatRoomPanelLayout.createSequentialGroup()
+                        .addComponent(TrennenButton)
+                        .addGap(28, 28, 28)
+                        .addComponent(OnlineClientsLabel)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(OnlineClientsScrollPane, javax.swing.GroupLayout.DEFAULT_SIZE, 277, Short.MAX_VALUE)))
+                .addGap(18, 18, 18)
+                .addComponent(StatusSeparator, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(3, 3, 3)
+                .addComponent(StatusLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 15, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(2, 2, 2))
+        );
+
+        ChatRoomTabbedPane.addTab("Chat Room", ChatRoomPanel);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addComponent(ChatRoomTabbedPane)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addComponent(ChatRoomTabbedPane)
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void SendenButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_SendenButtonActionPerformed
+        if ( !MessageTextField.getText().equals("") ) {
+            MessageHistoryTextPane.setText(MessageHistoryTextPane.getText() + "\n" + MessageTextField.getText());
+            MessageTextField.setText("");
+        }
+    }//GEN-LAST:event_SendenButtonActionPerformed
+
+    private void TrennenButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_TrennenButtonActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_TrennenButtonActionPerformed
 
     /**
      * @param args the command line arguments
@@ -79,5 +193,17 @@ public class ApplicationFrame extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JPanel ChatRoomPanel;
+    private javax.swing.JTabbedPane ChatRoomTabbedPane;
+    private javax.swing.JScrollPane MessageHistoryScrollPane;
+    private javax.swing.JTextPane MessageHistoryTextPane;
+    private javax.swing.JTextField MessageTextField;
+    private javax.swing.JLabel OnlineClientsLabel;
+    private javax.swing.JList OnlineClientsList;
+    private javax.swing.JScrollPane OnlineClientsScrollPane;
+    private javax.swing.JButton SendenButton;
+    private javax.swing.JLabel StatusLabel;
+    private javax.swing.JSeparator StatusSeparator;
+    private javax.swing.JButton TrennenButton;
     // End of variables declaration//GEN-END:variables
 }
