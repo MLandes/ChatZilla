c36c0795e9b0983af81ae59ede1ef11366d92248
Lukas Hurtz <lukas@am-nordpark.de>
2015-04-23 15:10:27 +0200

    CLientSocketThread implementiert

--- Server/src/main/java/zillacorp/server/ClientSocketThread.java
+++ Server/src/main/java/zillacorp/server/ClientSocketThread.java
@@ -5,14 +5,51 @@
  */
 package zillacorp.server;
 
+import com.google.gson.Gson;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+import zillacorp.model.Message;
+
 /**
  *
  * @author l.hurtz
  */
 public class ClientSocketThread extends Thread implements Runnable
 {
+    Scanner inputFromClient;
+    PrintWriter outputToClient;
+    String userNickname;
+    
+    public ClientSocketThread(Socket clientSocket) throws IOException
+    {
+        inputFromClient = new Scanner(clientSocket.getInputStream());
+        outputToClient = new PrintWriter(clientSocket.getOutputStream());
+        userNickname = inputFromClient.nextLine();
+    }
+    
+    @Override
     public void run()
     {
-        //TODO
+        String newMessageText;
+        while(true)
+        {
+            Message newMessage = new Message();
+            
+            newMessageText = inputFromClient.nextLine();  
+            
+            newMessage.message = newMessageText;
+            newMessage.userNickname = userNickname;
+            
+            ServerThread.messagesFromClients.add(newMessage);
+        }
+    }
+    
+    public void sendMessageToClient(Message message)
+    {
+        String serializesMessage = new Gson().toJson(message);
+        
+        outputToClient.println(serializesMessage);
     }
 }
