1a20a3b1187b203a579d23c24e231fb2b83e8483
Lukas Hurtz <lukas@am-nordpark.de>
2015-04-28 20:19:32 +0200

    neue Klasse für History Requests hinzugefügt

---
+++ Model/src/main/java/zillacorp/socketModel/HistoryRequest.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package zillacorp.socketModel;
+
+/**
+ *
+ * @author l.hurtz
+ */
+public class HistoryRequest 
+{
+    public long historyTimestamp;
+}
